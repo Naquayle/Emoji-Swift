a274766ae4f629f393d229a560ffde2cc01d32d3
Nicholas Quayle <naquayle@gmail.com>
2017-03-01 11:45:55 -0500

    -Add an escapePrefixAndSuffix property to String that lets the user configure what string is used to surround the escaped emoji (e.g. setting the escapePrefixAndSuffix string to be “+” would make 🐶 escape to “+dog+”)

--- Sources/String+Emoji.swift
+++ Sources/String+Emoji.swift
@@ -17,11 +17,18 @@ extension String {
         }
     }
 
+    public static var escapePrefixAndSuffix = ":" {
+        didSet {
+            emojiUnescapeRegExp = createEmojiUnescapeRegExp()
+            emojiEscapeRegExp = createEmojiEscapeRegExp()
+        }
+    }
+
     fileprivate static var emojiUnescapeRegExp = createEmojiUnescapeRegExp()
     fileprivate static var emojiEscapeRegExp = createEmojiEscapeRegExp()
 
     fileprivate static func createEmojiUnescapeRegExp() -> NSRegularExpression {
-        return try! NSRegularExpression(pattern: emojiDictionary.keys.map { ":\($0):" } .joined(separator: "|"), options: [])
+        return try! NSRegularExpression(pattern: emojiDictionary.keys.map { "\(escapePrefixAndSuffix)\($0)\(escapePrefixAndSuffix)" } .joined(separator: "|"), options: [])
     }
 
     fileprivate static func createEmojiEscapeRegExp() -> NSRegularExpression {
@@ -46,12 +53,13 @@ extension String {
     public var emojiEscapedString: String {
         var s = self as NSString
         let ms = String.emojiEscapeRegExp.matches(in: self, options: [], range: NSMakeRange(0, s.length))
+        let escapePrefixAndSuffix = String.escapePrefixAndSuffix
         ms.reversed().forEach { m in
             let r = m.range
             let p = s.substring(with: r)
             let fs = String.emojiDictionary.lazy.filter { $0.1 == p }
             if let kv = fs.first {
-                s = s.replacingCharacters(in: r, with: ":\(kv.0):") as NSString
+                s = s.replacingCharacters(in: r, with: "\(escapePrefixAndSuffix)\(kv.0)\(escapePrefixAndSuffix)") as NSString
             }
         }
         return s as String
